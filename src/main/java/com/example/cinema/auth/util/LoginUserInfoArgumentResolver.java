26ebb681b49c4aa318456de4c3bf76fdefe5d64d
chkim <cndghks0213@gmail.com>
2022-06-07 23:08:01 +0900

    feat: security context holder에서 회원의 정보를 추출해 argument로 변환하는 기능 구현

---
+++ src/main/java/com/example/cinema/auth/util/LoginUserInfoArgumentResolver.java
@@ -0,0 +1,32 @@
+package com.example.cinema.auth.util;
+
+import com.example.cinema.auth.domain.MemberAdapter;
+import com.example.cinema.member.domain.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Slf4j
+@RequiredArgsConstructor
+public class LoginUserInfoArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasMemberInfoAnnotation = parameter.hasParameterAnnotation(LoginUser.class);
+        boolean hasMemberType = Member.class.isAssignableFrom(parameter.getParameterType());
+        return hasMemberInfoAnnotation && hasMemberType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        MemberAdapter memberAdapter = (MemberAdapter) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+        return memberAdapter.getMember();
+    }
+}
