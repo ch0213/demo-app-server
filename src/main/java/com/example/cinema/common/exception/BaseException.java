027c951c340a6efb545c3700e865d08f54491b18
chkim <cndghks0213@gmail.com>
2022-06-07 23:02:34 +0900

    feat: 모든 예외가 상속할 BaseException 정의

---
+++ src/main/java/com/example/cinema/common/exception/BaseException.java
@@ -0,0 +1,15 @@
+package com.example.cinema.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class BaseException extends RuntimeException {
+    private String message;
+    private LocalDateTime timestamp;
+    private HttpStatus status;
+}
