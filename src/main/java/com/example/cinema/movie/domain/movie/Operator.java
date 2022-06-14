0f4cc29d690faee6cb781d774d3d524bdb390729
chkim <cndghks0213@gmail.com>
2022-06-14 23:40:57 +0900

    feat: 영화 검색 시 사용되는 조건

---
+++ src/main/java/com/example/cinema/movie/domain/movie/Operator.java
@@ -0,0 +1,10 @@
+package com.example.cinema.movie.domain.movie;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Operator {
+    AND, OR;
+}
