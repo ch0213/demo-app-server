d68a67a8bfd5c70a639ab2cf789cf1365c985630
chkim <cndghks0213@gmail.com>
2022-06-16 23:50:07 +0900

    feat: 관람 날짜, 제목으로 영화를 검색하는 기능 구현

--- src/main/java/com/example/cinema/movie/infrastructure/MovieCustomRepository.java
+++ src/main/java/com/example/cinema/movie/infrastructure/MovieCustomRepository.java
@@ -1,12 +1,11 @@
 package com.example.cinema.movie.infrastructure;
 
 import com.example.cinema.movie.domain.movie.Movie;
-import com.example.cinema.movie.domain.movie.Operator;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
 
 public interface MovieCustomRepository {
-    Page<Movie> findByMovieTitleAndDate(Pageable pageable, String title, LocalDate date, Operator operator);
+    Page<Movie> findByMovieTitleAndDate(Pageable pageable, String title, LocalDate date);
 }
--- src/main/java/com/example/cinema/movie/infrastructure/MovieCustomRepositoryImpl.java
+++ src/main/java/com/example/cinema/movie/infrastructure/MovieCustomRepositoryImpl.java
@@ -1,55 +1,50 @@
 package com.example.cinema.movie.infrastructure;
 
 import com.example.cinema.movie.domain.movie.Movie;
-import com.example.cinema.movie.domain.movie.Operator;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.util.StringUtils;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import static com.example.cinema.movie.domain.movie.Operator.OR;
 import static com.example.cinema.movie.domain.movie.QMovie.movie;
 
+@Slf4j
 @RequiredArgsConstructor
 public class MovieCustomRepositoryImpl implements MovieCustomRepository {
 
     private final JPAQueryFactory factory;
 
     @Override
-    public Page<Movie> findByMovieTitleAndDate(Pageable pageable, String title, LocalDate date, Operator operator) {
+    public Page<Movie> findByMovieTitleAndDate(Pageable pageable, String title, LocalDate date) {
         List<Movie> movies = factory.selectFrom(movie)
-                .where(checkCondition(title, date, operator), movie.end.goe(LocalDate.now()))
+                .where(checkCondition(title, date))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
+                .orderBy(movie.start.asc(), movie.title.asc())
                 .fetch();
         return PageableExecutionUtils.getPage(movies, pageable, movies::size);
     }
 
-    private BooleanExpression checkCondition(String title, LocalDate date, Operator operator) {
-        if (title == null && date == null) {
-            return bound(LocalDate.now());
+    private BooleanExpression checkCondition(String title, LocalDate date) {
+        if (!StringUtils.hasText(title) && date != null) {
+            return movie.start.loe(date).and(movie.end.goe(date));
         }
 
-        if (title == null) {
-            return bound(date);
+        if (StringUtils.hasText(title) && date == null) {
+            return movie.title.contains(title).and(movie.end.goe(LocalDate.now()));
         }
 
-        if (date == null) {
-            return movie.title.contains(title).and(bound(LocalDate.now()));
+        if (StringUtils.hasText(title) && date != null) {
+            return movie.title.contains(title).and(movie.start.loe(date).and(movie.end.goe(date)));
         }
 
-        if (operator == OR) {
-            return movie.title.contains(title).or(bound(date));
-        }
-        return movie.title.contains(title).and(bound(date));
-    }
-
-    private BooleanExpression bound(LocalDate date) {
-        return movie.start.loe(date).and(movie.end.goe(date));
+        return movie.end.goe(LocalDate.now());
     }
 }
