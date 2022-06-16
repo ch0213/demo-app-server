a9ab1c639f64066518ef843785cd207ddb0a91f3
chkim <cndghks0213@gmail.com>
2022-06-16 23:51:01 +0900

    refactor: 영화 검색 조건에서 연산자 제외

--- src/main/java/com/example/cinema/movie/presentation/dto/FindMovieRequest.java
+++ src/main/java/com/example/cinema/movie/presentation/dto/FindMovieRequest.java
@@ -1,6 +1,5 @@
 package com.example.cinema.movie.presentation.dto;
 
-import com.example.cinema.movie.domain.movie.Operator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -15,6 +14,4 @@ public class FindMovieRequest {
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
-
-    private Operator operator;
 }
