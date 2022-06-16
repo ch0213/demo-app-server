ee944e47f86687cf4c61fcd4fbbc3e74bd096e10
chkim <cndghks0213@gmail.com>
2022-06-16 23:49:42 +0900

    feat: directorRepository 생성

---
+++ src/main/java/com/example/cinema/movie/domain/director/DirectorRepository.java
@@ -0,0 +1,6 @@
+package com.example.cinema.movie.domain.director;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DirectorRepository extends JpaRepository<Director, Long> {
+}
