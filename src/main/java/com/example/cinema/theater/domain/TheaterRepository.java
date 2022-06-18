011def83883bcfcc371934ee5922446c570586b2
chkim <cndghks0213@gmail.com>
2022-06-18 23:56:46 +0900

    feat: 상영관 repository

---
+++ src/main/java/com/example/cinema/theater/domain/TheaterRepository.java
@@ -0,0 +1,6 @@
+package com.example.cinema.theater.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TheaterRepository extends JpaRepository<Theater, Long> {
+}
